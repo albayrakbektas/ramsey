c32d8ee67e8b9e48d7c1f3f3091ab3f6921b7d00
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-22 03:10:50 +0300

    update state

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,36 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    isMenuOpen: false,
+    iconList: [
+      {
+        href: "#",
+        src: "fa-brands fa-twitter",
+        alt: "twitter",
+      },
+      {
+        href: "#",
+        src: "fa-brands fa-facebook-f",
+        alt: "twitter",
+      },
+      {
+        href: "#",
+        src: "fa-brands fa-google-plus-g",
+        alt: "twitter",
+      },
+      {
+        href: "#",
+        src: "fa-brands fa-linkedin",
+        alt: "twitter",
+      },
+      {
+        href: "#",
+        src: "fa-solid fa-basketball",
+        alt: "twitter",
+      },
+    ],
+  },
   getters: {},
   mutations: {},
   actions: {},
