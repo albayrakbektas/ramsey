0fc77a77d12d02c1a99cab3c69893454d56bdc6d
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-22 03:12:19 +0300

    change list query

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,52 +3,17 @@
     <div class="left">
       <p>© Ramsay 2020.</p>
     </div>
-    <div class="right">
-      <a
-        v-for="(item, index) of iconList"
-        target="_blank"
-        :href="item.href"
-        :key="index"
-      >
-        <i :class="item.src"></i>
-      </a>
-    </div>
+    <FooterIcons />
   </div>
 </template>
 
 <script>
+import FooterIcons from "@/components/FooterIcons";
 export default {
   name: "AppFooter",
+  components: { FooterIcons },
   data() {
-    return {
-      iconList: [
-        {
-          href: "#",
-          src: "fa-brands fa-twitter",
-          alt: "twitter",
-        },
-        {
-          href: "#",
-          src: "fa-brands fa-facebook-f",
-          alt: "twitter",
-        },
-        {
-          href: "#",
-          src: "fa-brands fa-google-plus-g",
-          alt: "twitter",
-        },
-        {
-          href: "#",
-          src: "fa-brands fa-linkedin",
-          alt: "twitter",
-        },
-        {
-          href: "#",
-          src: "fa-solid fa-basketball",
-          alt: "twitter",
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
